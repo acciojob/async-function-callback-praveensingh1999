3e2f39df02c583df89f4a7e98b15be1a547d5d45
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-16 14:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
-//your JS code here. If required.
+// your JS code here
+async function fetchData(url, callback) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        callback(data);
+    } catch (error) {
+        console.error("Error fetching:", error);
+    }
+}
+
+fetchData(url, (data) => {
+    console.log(data);
+
+    const output = document.querySelector("#output");
+    output.innerText = JSON.stringify(data, null, 2);
+    output.style.display = "block";
+});
